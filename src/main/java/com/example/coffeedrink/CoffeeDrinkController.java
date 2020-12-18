24ed53a8f269454f2070c38a8ddf6d6e70cff4e8
Aboud Chamoun <aboud.chamoun@yahoo.com>
2020-12-18 15:23:23 +0100

    Building Controller/Get Drink local host test succeeded

--- src/main/java/com/example/coffeedrink/CoffeeDrinkController.java
+++ src/main/java/com/example/coffeedrink/CoffeeDrinkController.java
@@ -1,17 +1,22 @@
 package com.example.coffeedrink;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.coffeedrink.response.GetCoffeeDrinkIngredientsResponse;
+
+import com.example.coffeedrink.functions.CoffeeDrinkFinder;
 import com.example.coffeedrink.functions.IngredientFinder;
 import com.example.coffeedrink.pojo.Ingredient;
+import com.example.coffeedrink.response.GetCoffeeDrinkIngredientsResponse;
+import com.example.coffeedrink.response.GetCoffeeDrinkResponse;
 
 @RestController
 public class CoffeeDrinkController {
 
 	IngredientFinder ingredientFinder = new IngredientFinder();
+	CoffeeDrinkFinder coffeeDrinkFinder = new CoffeeDrinkFinder();
 
 	@GetMapping("/webservices/getCoffeeDrinkIngredients")
 	public GetCoffeeDrinkIngredientsResponse getCoffeeDrinkIngredients(@RequestParam String coffeeDrink) {
@@ -20,6 +25,16 @@ public class CoffeeDrinkController {
 		for (Ingredient ingredient : ingredientFinder.findIngredients(coffeeDrink)) {
 			response.getResponseIngredientList().add(ingredient.toString());
 		}
+		return response;
+	}
+
+	@GetMapping("/webservices/getCoffeeDrink")
+	public GetCoffeeDrinkResponse getCoffeeDrink(@RequestParam String ingredient1, @RequestParam String ingredient2) {
+		GetCoffeeDrinkResponse response = new GetCoffeeDrinkResponse();
+		ArrayList<String> ingredientList = new ArrayList<String>();
+		ingredientList.add(ingredient1);
+		ingredientList.add(ingredient2);
+		response.setResponseCoffeeDrink(coffeeDrinkFinder.getCoffeeDrink(ingredientList).getCoffeeDrink().toString());
 		return response;
 	}
 }
---
+++ src/main/java/com/example/coffeedrink/response/GetCoffeeDrinkResponse.java
@@ -0,0 +1,21 @@
+package com.example.coffeedrink.response;
+
+public class GetCoffeeDrinkResponse {
+
+	String responseCoffeeDrink;
+
+	public GetCoffeeDrinkResponse() {
+
+		this.responseCoffeeDrink = "";
+	}
+
+	public void setResponseCoffeeDrink(String responseCoffeeDrink) {
+		this.responseCoffeeDrink = responseCoffeeDrink;
+
+	}
+
+	public String getResponseCoffeeDrink() {
+		return responseCoffeeDrink;
+	}
+
+}
